b0d73a64eb3c579ed7f142f1763775de079ec3d2
Glitch (nananbot) <none>
2018-03-19 17:03:28 +0000

    😳🎭 Updated with Glitch

--- bot.js
+++ bot.js
@@ -1,3 +1,16 @@
+const express = require('express')
+const app = express()
+const http = require('http');
+
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
