ac183d778bad53efdb1e2993101103fb3b22ef68
Tom Landwehr <landwehr.perlin@freenet.de>
2018-03-19 21:45:57 +0100

    implemented basic google sheets api

--- commands/rps.js
+++ commands/rps.js
@@ -1,15 +1,28 @@
 module.exports = {
     args: true,
+    usage: "<option>",
     name: 'rps',
     description: 'Rock, Paper, Scissors',
+    cooldown: 15,
     execute(message, args) {
        
         var items = Array("rock", "paper", "scissors");
 
 		var item = items[Math.floor(Math.random()*items.length)];
+		var emoji;
 
-		console.log("Yes");
-		console.log("Item: " + item);
+		if (item === "rock")
+		{
+			emoji = ":fist:";
+		}
+		else if (item === "paper")
+		{
+			emoji = ":hand_splayed:"
+		}
+		else if (item === "scissors")
+		{
+			emoji = ":scissors:"
+		}
 
 		if (args[0] != "rock" && args[0] != "paper" && args[0] != "scissors")
 		{
@@ -18,17 +31,17 @@ module.exports = {
 
 		else if (args[0] === item)
 		{
-			message.channel.send('We both picked ' +item+ ' and tied!');
+			message.channel.send('We both picked ' +emoji+ ' and tied!');
 		}
 
 		else if (args[0] === "rock" && item === "scissors" || args[0] === "scissors" && item === "paper" || args[0] === "paper" && item === "rock")
 		{
-			message.channel.send('I picked ' +item+ ' and lost!');
+			message.channel.send('I picked ' +emoji+ ' and lost!');
 		}
 
 		else if (args[0] === "rock" && item === "paper" || args[0] === "scissors" && item === "rock" || args[0] === "paper" && item === "scissors")
 		{
-			message.channel.send('I picked ' +item+ ' and won!');
+			message.channel.send('I picked ' +emoji+ ' and won!');
 		}
 
 
